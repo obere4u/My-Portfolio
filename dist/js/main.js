1c4c575639b82e55e75b4dd98c0795fd960c1a73
Nwosa Tochukwu <54867289+obere4u@users.noreply.github.com>
2022-09-30 00:06:24 -0700

    WIth SCSS
    Created a portfolio with SCSS

---
+++ dist/js/main.js
@@ -0,0 +1,27 @@
+
+const menuBtn = document.querySelector('.menu-btn');
+const hamBurger = document.querySelector('.menu-btn_burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItem = document.querySelectorAll('.menu-nav_item');
+
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        hamBurger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        navItem.forEach(item => item.classList.add('open'));
+        showMenu = true;
+    } else {
+        hamBurger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        navItem.forEach(item => item.classList.remove('open'));
+        showMenu = false;
+    }
+}
